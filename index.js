a8a666e67e8def6ea773366c4c4841a50ed224c1
Lidian Vilazio <lidianvilazio@hotmail.com>
2018-01-04 20:57:17 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,19 @@
+function breakOut(array, changeValue, stopValue) {
+  for(var i = 0; i < array.length; i++) {
+    if(array[i] === changeValue) {
+      continue
+    }
+    array[i] = changeValue
+  }
+  return array
+}
+
+
+function keepGoing(array, changeValue, skipValue) {
+
+}
+
+
+function findBy(array, findFn) {
+
+}
